17fc62c778cbd249d7c11ac76cdc925381877443
Junfeng Zhuang <pp026pp@gmail.com>
2015-05-26 20:48:12 -0700

    added bootstrap and backbone gems and initial generates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require turbolinks
+//= require underscore
+//= require backbone
+//= require sfmovies
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
---
+++ app/assets/javascripts/sfmovies.js
@@ -0,0 +1,13 @@
+window.Sfmovies = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Sfmovies.initialize();
+});
